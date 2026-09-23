b05f13347f3925a643909d338cc19a69a04c275e
exe <69010996@kmitl.ac.th>
2026-09-23 14:06:31 +0700

    feature/farewell

--- Hello.c
+++ Hello.c
@@ -2,10 +2,16 @@
 int main(void)
 {
 greeting(“Developer A,B,C !!”);
-return 0;
-} // Print a greeting message for the specified user
+farewell(“Developer A,B,C”);
+return 0;}
+ // Print a greeting message for the specified user
 int greeting(const char *name)
 {
 printf("Hello, %s!\n", name);
+return 0;
+}
+int farewell(const char *name)
+{
+printf("Goodbye, %s!\n", name);
 return 0;
 }
